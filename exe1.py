ad261831b5dc9ee292140034f2019b48bfcdc7a3
Chubaness <36613417+Chubaness@users.noreply.github.com>
2018-02-19 10:55:22 +0800

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,4 @@
+money = input ("Green paper you have?")
+print ("Dont have enough money BOI"), money < 15000 
+print ("Get yo sorry butt a PS4"), money > 15000
+print "You need", money + 5000, "if you want some more good stuff"
